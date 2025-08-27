5cb1d955cc89c7d92a70d32b9159e2ddc075e086
Saifullah <saifullah.saifullah@dfki.uni-kl.de>
2025-08-27 22:50:59 +0200

    added linting.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -80,37 +80,6 @@ function ArrowDownIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
   )
 }
 
-function Article({ article }: { article: ArticleWithSlug }) {
-  return (
-    <Card as="article">
-      <Card.Title href={`/articles/${article.slug}`}>
-        {article.title}
-      </Card.Title>
-      <Card.Eyebrow as="time" dateTime={article.date} decorate>
-        {formatDate(article.date)}
-      </Card.Eyebrow>
-      <Card.Description>{article.description}</Card.Description>
-      <Card.Cta>Read article</Card.Cta>
-    </Card>
-  )
-}
-
-
-function Publication({ publication }: { publication: PublicationWithSlug }) {
-  return (
-    <Card as="publication">
-      <Card.Title href={`/publications/${publication.slug}`}>
-        {publication.title}
-      </Card.Title>
-      <Card.Eyebrow as="time" dateTime={publication.date} decorate>
-        {formatDate(publication.date)}
-      </Card.Eyebrow>
-      <Card.Description>{publication.description}</Card.Description>
-      <Card.Cta>Read publication</Card.Cta>
-    </Card>
-  )
-}
-
 function SocialLink({
   icon: Icon,
   ...props
